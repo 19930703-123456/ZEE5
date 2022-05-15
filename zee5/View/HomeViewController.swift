642f1f26f3e19f75be525339680a61202dc92b75
Sasidhar-1993 <sasidharreddy7773@gmail.com>
2022-05-15 10:51:59 +0530

    update

--- zee5/View/HomeViewController.swift
+++ zee5/View/HomeViewController.swift
@@ -15,7 +15,13 @@ class HomeViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
-
+    
+    @IBAction func buttonClicked(_ sender: Any) {
+        var _ : UIStoryboard
+        let vc = storyboard?.instantiateViewController(withIdentifier: "MoreViewController") as? MoreViewController
+        self.navigationController?.pushViewController(vc!, animated: true)
+    }
+    
     /*
     // MARK: - Navigation
 
--- zee5/View/MoreViewController.swift
+++ zee5/View/MoreViewController.swift
@@ -12,14 +12,62 @@ class MoreViewController: UIViewController {
     @IBOutlet weak var navigationBar: UINavigationItem!
    
     @IBOutlet weak var emailTextField: UITextField!
+  
+    @IBOutlet weak var loginStackView: UIStackView!
+    
+    @IBOutlet weak var loginButton: UIButton!
+    
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        let email = emailTextField.text
+       
         let textAttributes = [NSAttributedString.Key.foregroundColor:UIColor.white]
         navigationController?.navigationBar.titleTextAttributes = textAttributes
+       
+       
+       
+    }
+     
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+      
+//        loginButton.isHidden = true
+        loginStackView.isHidden = false
+    }
+   
+
+    @IBAction func loginButtonClick(_ sender: Any) {
+        
+       
+        loginStackView.isHidden = false
+
+        guard let email = emailTextField.text, emailTextField.text?.count != 0  else {
+            loginButton.isHidden = false
+            loginStackView.isHidden = true
+          
+            return
+            
+        }
+        
+        if isValidEmail(emailID: emailTextField.text ?? "") == false {
+            loginButton.isHidden = false
+            loginStackView.isHidden = false
+            print("invaild")
+        } else {
+            loginStackView.isHidden = true
+            print("valid")}
+       
     }
     
-
+    func isValidEmail(emailID:String) -> Bool {
+       let emailRegEx = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}"
+       let emailTest = NSPredicate(format:"SELF MATCHES %@", emailRegEx)
+       return emailTest.evaluate(with: emailID)
+   }
     /*
     // MARK: - Navigation
 
