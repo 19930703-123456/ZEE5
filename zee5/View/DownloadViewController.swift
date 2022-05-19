bb53f4530e906f530cd37b89a7c24a04122f958b
Sasidhar-1993 <sasidharreddy7773@gmail.com>
2022-05-19 13:26:11 +0530

    updated

--- zee5/View/DownloadViewController.swift
+++ zee5/View/DownloadViewController.swift
@@ -8,27 +8,26 @@
 import UIKit
 
 class DownloadViewController: UIViewController {
-
+    
     @IBOutlet weak var profileImageView: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        profileImageView.layer.cornerRadius = profileImageView.bounds.width / 2
         profileImageView.layer.masksToBounds = true
-           profileImageView.layer.cornerRadius = profileImageView.bounds.width / 2
-
-        // Do any additional setup after loading the view.
+        
     }
     
-
+    
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
